a66c703ab3de65bd355c145cece8e9a8078df5fc
sayantanpal <sayantanpal100@gmail.com>
2026-01-04 04:14:06 +0530

    feat: Configure dotenv for environment variables and add nodemon dev script.

--- Sec 21 Building Complete Backend /index.js
+++ Sec 21 Building Complete Backend /index.js
@@ -1,1 +1,11 @@
+import dotenv from "dotenv";
+dotenv.config({
+    path: "./.env"
+});
+
+
+console.log(process.env.PORT);
+
+const PORT = process.env.PORT || 3000;
+
 console.log("starting the server...");
